3d833263f134dcddf9ce1c29b4de6f6579279633
Zibog <Zibog@github.com>
2021-11-07 23:08:21 +0300

    Add two more heuristics

--- ReversiBase/Solver.cs
+++ ReversiBase/Solver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReversiBase
 {
@@ -97,6 +98,87 @@ namespace ReversiBase
             if (maxMobility + minMobility > 0)
                 return 100 * (maxMobility - minMobility) / (maxMobility + minMobility);
             return 0;
+        }
+
+        public static int CornersHeuristic(Game game, TileColor color)
+        {
+            var currentPlayer = game.IsPlayerBlack ? TileColor.Black : TileColor.White;
+            var corners = new List<Tuple<int, int>>
+            {
+                Tuple.Create(0, 0),
+                Tuple.Create((int)Board.Size - 1, 0),
+                Tuple.Create(0, (int)Board.Size - 1),
+                Tuple.Create((int)Board.Size - 1, (int)Board.Size - 1)
+            };
+
+            var maxScore = 0;
+            var minScore = 0;
+
+            foreach (var corner in corners)
+            {
+                var t = game.Board[corner.Item1, corner.Item2];
+                if (t != null)
+                {
+                    if (t.Color == color)
+                        ++maxScore;
+                    else if (t.Color != TileColor.Blank)
+                        ++minScore;
+                }
+            }
+
+            if (maxScore + minScore > 0)
+                return 100 * (maxScore - minScore) / (maxScore + minScore);
+            return 0;
+        }
+
+        public static int WeightedHeuristic(Game game, TileColor color)
+        {
+            var weights = new int[(int)Board.Size, (int)Board.Size];
+            var score = 0;
+
+            #region Weights
+
+            weights[0, 0] = 4;
+            weights[0, 1] = -3;
+            weights[0, 2] = 2;
+            weights[0, 3] = 2;
+
+            weights[1, 0] = -3;
+            weights[1, 1] = -4;
+            weights[1, 2] = -1;
+            weights[1, 3] = -1;
+
+            weights[2, 0] = 2;
+            weights[2, 1] = -1;
+            weights[2, 2] = 1;
+            weights[2, 3] = 0;
+
+            weights[3, 0] = 2;
+            weights[3, 1] = -1;
+            weights[3, 2] = 0;
+            weights[3, 3] = 1;
+
+            #endregion
+
+            for (int i = 0; i < Board.Size; i++)
+            {
+                for (int j = 0; j < Board.Size; j++)
+                {
+                    var im = i < 4 ? i : 3 - i % 4;
+                    var jm = j < 4 ? j : 3 - j % 4;
+
+                    var t = game.Board[i, j];
+                    if (t != null)
+                    {
+                        if (t.Color == color)
+                            score += weights[im, jm];
+                        else if (t.Color != TileColor.Blank)
+                            score -= weights[im, jm];
+                    }
+                }
+            }
+
+            return score;
         }
 
         #endregion
